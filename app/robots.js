e963c14a6c3f9b33369d68737a33d583c9d8ae71
Muhammad-Umer34 <umer.gm89@gmail.com>
2026-07-20 22:48:26 +0500

    chore(seo): set domain to https://muhammad-umer-portfolio-phi.vercel.app

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,5 @@
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://muhammad-umer.vercel.app';
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://muhammad-umer-portfolio-phi.vercel.app';
 
   return {
     rules: {
